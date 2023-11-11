c590a9e81c8f8778fb474eb76702cce0496e567b
imkrmin <zum2n2@gmail.com>
2023-11-11 22:48:34 +0900

    Feat: 로그인 및 회원가입 js 파일 추가

---
+++ 2-Weekly-Mission/signin.js
@@ -0,0 +1,5 @@
+/** 로그인 */
+const InputUseremail = document.querySelector('#sign-email');
+const InputPassword = document.querySelector('#sign-password');
+
+
---
+++ 2-Weekly-Mission/signup.js
@@ -0,0 +1,15 @@
+/** 회원가입 */
+const InputUseremail = document.querySelector('#sign-email');
+const InputPassword = document.querySelector('#sign-password');
+const InputPasswordAgain = document.querySelector('#sign-password-again');
+
+
+
+
+
+
+
+
+
+
+
