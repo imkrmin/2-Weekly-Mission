737577e3dca33b51f1ca18c1f8f15f62dcc8365f
imkrmin <zum2n2@gmail.com>
2023-11-12 02:22:42 +0900

    feat: 로그인 이메일 유효성검사 기능 추가

--- 2-Weekly-Mission/signin.js
+++ 2-Weekly-Mission/signin.js
@@ -1,5 +1,29 @@
 /** 로그인 */
-const InputUseremail = document.querySelector('#sign-email');
-const InputPassword = document.querySelector('#sign-password');
+const InputUserEmail = document.querySelector("#sign-email");
+const noneMessageEmail = document.querySelector(".none-message-email");
+const wrongMessageEmail = document.querySelector(".wrong-message-email");
 
+function validateEmail() {
+  const userEmail = InputUserEmail.value;
+  if (userEmail === "") {
+    noneMessageEmail.style.color = "red";
+    noneMessageEmail.style.display = "block";
+    InputUserEmail.style.border = "1px solid var(--linkbrary-red, #FF5B56)";
+  } else if (!isValidEmail()) {
+    noneMessageEmail.style.display = "none";
+    wrongMessageEmail.style.color = "red";
+    wrongMessageEmail.style.display = "block";
+    InputUserEmail.style.border = "1px solid var(--linkbrary-red, #FF5B56)";
+  } else {
+    noneMessageEmail.style.display = "none";
+    wrongMessageEmail.style.display = "none";
+  }
+}
 
+function isValidEmail() {
+  let regExp =
+    /^[0-9a-zA-Z]([-_\.]?[0-9a-zA-Z])*@[0-9a-zA-Z]([-_\.]?[0-9a-zA-Z])*\.[a-zA-Z]{2,3}$/i;
+  return regExp.test(InputUserEmail.value);
+}
+
+InputUserEmail.addEventListener("focusout", validateEmail);
