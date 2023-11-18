6fa8b25477e0e7c7fcddda0f306448dbb8396a78
imkrmin <zum2n2@gmail.com>
2023-11-18 21:41:42 +0900

    feat: api 연동

---
+++ 2-Weekly-Mission/src/signin.js
@@ -0,0 +1,103 @@
+import {
+  setInputError,
+  removeInputError,
+  isEmailValid,
+  togglePassword,
+  TEST_USER,
+} from "./utils.js";
+
+const emailInput = document.querySelector("#email");
+const emailErrorMessage = document.querySelector("#email-error-message");
+emailInput.addEventListener("focusout", event =>
+  validateEmailInput(event.target.value)
+);
+function validateEmailInput(email) {
+  if (email === "") {
+    setInputError(
+      { input: emailInput, errorMessage: emailErrorMessage },
+      "이메일을 입력해주세요."
+    );
+    return;
+  }
+  if (!isEmailValid(email)) {
+    setInputError(
+      { input: emailInput, errorMessage: emailErrorMessage },
+      "올바른 이메일 주소가 아닙니다."
+    );
+    return;
+  }
+  removeInputError({ input: emailInput, errorMessage: emailErrorMessage });
+}
+
+const passwordInput = document.querySelector("#password");
+const passwordErrorMessage = document.querySelector("#password-error-message");
+passwordInput.addEventListener("focusout", event =>
+  validatePasswordInput(event.target.value)
+);
+function validatePasswordInput(password) {
+  if (password === "") {
+    setInputError(
+      { input: passwordInput, errorMessage: passwordErrorMessage },
+      "비밀번호를 입력해주세요."
+    );
+    return;
+  }
+  removeInputError({
+    input: passwordInput,
+    errorMessage: passwordErrorMessage,
+  });
+}
+
+const passwordToggleButton = document.querySelector("#password-toggle");
+passwordToggleButton.addEventListener("click", () =>
+  togglePassword(passwordInput, passwordToggleButton)
+);
+
+const signForm = document.querySelector("#form");
+signForm.addEventListener("submit", submitForm);
+function submitForm(event) {
+  event.preventDefault();
+
+  const isTestUser =
+    emailInput.value === TEST_USER.email &&
+    passwordInput.value === TEST_USER.password;
+
+  if (isTestUser) {
+    location.href = "/folder";
+    return;
+  }
+  setInputError(
+    { input: emailInput, errorMessage: emailErrorMessage },
+    "이메일을 확인해주세요."
+  );
+  setInputError(
+    { input: passwordInput, errorMessage: passwordErrorMessage },
+    "비밀번호를 확인해주세요."
+  );
+}
+
+// signin api 보내기
+const API_URL = "https://bootcamp-api.codeit.kr/api";
+
+async function signinSuccess() {
+  try {
+    const response = await fetch(`${API_URL}/sign-in`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(),
+    });
+    const responseJson = await response.json();
+    console.log(responseJson);
+    //   if (responseJson.status === 200) {
+    //     window.location.href = "/folder.html";
+    //   }
+    //   // console.log(responseJson);
+  } catch (error) {
+    // 오류 처리
+    console.log("오류 발생:");
+  }
+}
+
+signinSuccess();
